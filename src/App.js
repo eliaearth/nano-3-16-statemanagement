94e1b6bcfd03c46f94b046b6d50aba0c2286a64a
eliaearth <elia_earth@yahoo.com>
2018-12-13 12:41:51 +0000

    Add fuctionality as requested in the instructions.md

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserAddContainer from './UserAddContainer';
+import UserListContainer from './UserListContainer';
 
 /*
 This exercise will help you put together and practice all of the concepts you've
@@ -11,6 +13,35 @@ The instructions for this project are located in the `instructions.md` file.
 */
 
 class App extends Component {
+  state = {
+    users: [],
+    newUser: {}
+  };
+ addUser = event => {
+   event.preventDefault();
+   const uFirstNAme = event.target.userFirstName.value;
+   const uLastName = event.target.userLastName.value;
+   const uName = event.target.userName.value;
+   const newUser = {
+     firstName: uFirstNAme, 
+     lastName: uLastName, 
+     userName: uName,
+     games: 0
+   };
+   const that = this;
+   that.setState((prevState) => 
+    {
+     const userAlreadyExists = prevState.users.find(user => user.userName === newUser.userName);
+     if( !userAlreadyExists ) {
+       return {
+             users:[...prevState.users, newUser]
+       }
+     }else { 
+       window.alert("User name must be unique!");
+     }
+    });
+ }
+ 
   render() {
     return (
       <div className="App">
@@ -18,6 +49,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
+		<UserAddContainer handleAdd={this.addUser}/>
+		<UserListContainer users={this.state.users} />
       </div>
     );
   }
